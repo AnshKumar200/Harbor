a8a9b600a8ec9214715d4dadef13b153d3e7b817
Ansh Kumar <ansh0kumar07@gmail.com>
2026-05-04 23:09:37 +0530

    isolate mount namespace

--- pkg/runtime.go
+++ pkg/runtime.go
@@ -34,7 +34,10 @@ func Run() {
 	cmd := exec.Command("/proc/self/exe", args...)
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	// cmd.SysProcAttr = &syscall.SysProcAttr{
